6543737e199c19a8ca1b20583735fe73e9890339
Raushan0502 <rk7250846@gmail.com>
2025-03-28 02:44:26 +0530

    modified data version2 and saved indvc remote folder s3

--- demo.py
+++ demo.py
@@ -9,13 +9,18 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
-# # Adding new row to df for V2
-new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+# # # Adding new row to df for
+# new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+# df.loc[len(df.index)] = new_row_loc
+
+# # # Adding new row to df for 
+# new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+# df.loc[len(df.index)] = new_row_loc2
+
+ # Adding new row to df for v2
+new_row_loc = {'Name': 'mahi', 'Age': 25, 'City': 'Kurtha, gaya'}
 df.loc[len(df.index)] = new_row_loc
 
-# # Adding new row to df for V3
-new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
-df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
